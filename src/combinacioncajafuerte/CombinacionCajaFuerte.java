26283b10c051ee98e9cf0c04a3c10abbb8fde26c
Amarico <Amarico@equipo-zalman>
2018-09-10 20:08:43 +0200

    Inicio repositorio

---
+++ src/combinacioncajafuerte/CombinacionCajaFuerte.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package combinacioncajafuerte;
+
+/**
+ *
+ * @author Amarico
+ */
+public class CombinacionCajaFuerte {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+    
+    /**
+     * Realiza el control de acceso a una caja fuerte. La combinación será un
+     * número de 4 cifras. El programa nos pedirá la combinación para abrirla.
+     * Si no acertamos, se nos mostrará el mensaje “Lo siento, esa no es la
+     * combinación” y si acertamos se nos dirá “La caja fuerte se ha abierto
+     * satisfactoriamente”. Tendremos cuatro oportunidades para abrir la caja
+     * fuerte.
+     *
+     *
+     */
+  }
+  
+}
